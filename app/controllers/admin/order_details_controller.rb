4a3904cfc14cb90811f9e25a6071cf2c494a231f
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-11-01 06:15:03 +0000

    Complete requirements

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,21 +1,26 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order_detail = OrderDetail.find(params[:id])
     @order = Order.find(params[:order_id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_details = @order.order_details
+
+    is_updated = true
     if @order_detail.update(order_detail_params)
       @order.update(status: 2) if @order_detail.production_status == "making"
-    elsif @order_detail.production_status == "done"
-      if @order_detail.production_status[:done].count == @order_detail.count
-        @order.update(status: 3) 
+      @order_details.each do |order_detail|
+        if order_detail.production_status != "done"
+          is_updated = false
+        end
       end
+      @order.update(status: 3) if is_updated
       redirect_to request.referer
     else
       render 'orders/show'
     end
   end
-  
+
   private
-  
+
   def order_detail_params
     params.require(:order_detail).permit(:production_status)
   end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -24,10 +24,11 @@ class Public::OrdersController < ApplicationController
         order_details.order_id = @order.id
         order_details.purchase_price = cart_item.item.with_tax_price
         order_details.amount = cart_item.amount
+        order_details.production_status = 0
         order_details.save
       end
       current_customer.cart_items.destroy_all
-      redirect_to confirm_orders_path
+      redirect_to complete_orders_path
     else
       flash.now[:alert] = "注文が確定できませんでした。もう一度やり直してください。"
       render :new
@@ -51,6 +52,10 @@ class Public::OrdersController < ApplicationController
       @order.postal_code = address.postal_code
       @order.address = address.address
       @order.name = address.name
+    elsif params[:order][:address_option] == "2"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
     end
   end
 
@@ -58,6 +63,6 @@ class Public::OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :delivery_charge, :status, :total_pric)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :delivery_charge, :total_price, :status)
   end
 end
